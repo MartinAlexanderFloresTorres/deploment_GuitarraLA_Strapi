acf7ebc935d8afaa8807962705d131e5bb6e3a87
Martin Alexander Flores Torres <martinflorestorres21@gmail.com>
2022-07-03 00:33:25 -0500

    version 1

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '983a6e9282f119561df07679014337c9'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '55974720-3a7a-43f5-8826-0ff5858e5df8'
+};
